9b5a7fc72dead1e6c9383851dcacc7dad3210df0
patrick1994 <patrick.kelava.g.1994@gmail.com>
2026-08-06 21:13:57 +0200

    postquantum reputation system

--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pat/p2p/tradep2p2
-# Build directory: /home/pat/p2p/tradep2p2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[tradep2p_unit_tests]=] "/home/pat/p2p/tradep2p2/tradep2p_unit_tests")
-set_tests_properties([=[tradep2p_unit_tests]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/pat/p2p/tradep2p2/CMakeLists.txt;77;add_test;/home/pat/p2p/tradep2p2/CMakeLists.txt;0;")
